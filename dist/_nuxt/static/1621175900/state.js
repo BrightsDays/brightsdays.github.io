76bb8fd41f80530394548acf56737966c24fdabe
BrightsDays <brightsdayss@gmail.com>
2021-05-16 18:07:05 +0300

    fix payload problem

--- dist/_nuxt/static/1621175900/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c,d){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1621175900",layout:"default",error:a,state:{adaptive:{deviceType:"desktop"}},serverRendered:b,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},colorMode:{preference:d,value:d,unknown:b,forced:false}}}(null,true,"\u002F","system"));
