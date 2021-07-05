0246bb35ab19b27ccabfcc4b95ae7bc329485cd2
Viacheslav Ivanov <brightsdayss@gmail.com>
2021-07-05 17:15:59 +0300

    generate

---
+++ dist/_nuxt/static/1625494508/articles/old-school-deck-dead-guy-ale/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/articles/old-school-deck-dead-guy-ale", {data:[{}],fetch:{},mutations:[]});
---
+++ dist/_nuxt/static/1625494508/articles/standard-mono-black-aggro/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/articles/standard-mono-black-aggro", {data:[{}],fetch:{},mutations:[]});
