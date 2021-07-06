d156b2d48c04f7189c642390952b474494878c87
Viacheslav Ivanov <brightsdayss@gmail.com>
2021-07-06 17:07:05 +0300

    add link

--- components/Content.vue
+++ components/Content.vue
@@ -15,11 +15,14 @@
         li.list__item <i>2021</i> - <b>ExtTeam</b>, Frontend Developer
         li.list__item <i>2020-2021</i> - <b>AdCombo</b>, HTML/CSS Developer
 
-    Details.portfolio(:title="'Приложения'")
+    Details.portfolio(:title="'Портфолио'")
       ul.list
         li.list__item
-          a.link(href="https://brightsdays.github.io/mortgage-calculator/index.html" target="_blank" title="Mortage calculator" rel="noreferrer") Mortage calculator
-
+          a.link(href="https://brightsdays.github.io/mortgage-calculator/" target="_blank" title="Mortage calculator" rel="noreferrer") Ипотечный калькулятор
+          p.list__about Простое React-приложение, с помощью которого можно рассчитать ежемесячные затраты на выплату ипотечного обязательства.
+        li.list__item
+          a.link(href="https://brightsdays.github.io/nan-landing-page/" target="_blank" title="NAN Optipro" rel="noreferrer") Лендинг NAN Optipro
+          p.list__about Пример верстки рекламной страницы для производителя детского питания. Лендинг составлен из компонентов React.
     MyBlog
 </template>
 
