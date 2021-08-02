18c4088f80ef7b55a28be3054784ba8d565e9a44
Viacheslav Ivanov <brightsdayss@gmail.com>
2021-08-02 19:48:03 +0300

    change main content

--- components/page-content.vue
+++ components/page-content.vue
@@ -7,13 +7,19 @@
 
     Details(:title="'Обо мне'")
       p.paragraph Живу в Санкт-Петербурге, с начала 2020 работаю удаленно.
-      p.paragraph Работаю с <b>Vue/Pug/Sass</b> - но использовал и другие препроцессоры и шаблонизаторы, собирал проекты при помощи Gulp, писал на React.
+      p.paragraph Работаю с <b>Vue + Sass</b> - но использовал и другие сборщики, препроцессоры и шаблонизаторы; немного писал на React.
       p.paragraph В свободное время я учусь исполнять блюз на губной гармонике и играю в настольную игру 93 года выпуска.
 
     Details.work(:title="'Опыт работы'")
       ul.list
         li.list__item <i>2021</i> - <b>ExtTeam</b>, Frontend Developer
+          ul.work__info
+            p - работаю в настоящее время.
         li.list__item <i>2020-2021</i> - <b>AdCombo</b>, HTML/CSS Developer
+          ul.work__info
+            p - верстал посадочные страницы, адаптивно и кроссбраузерно;
+            p - использовал сборщик, шаблонизатор, препроцессы (Gulp, Pug, SASS/SCSS);
+            p - создавал слайдеры, навигацию, паралакс-эффекты и другие анимации.
 
     page-blog
 </template>
@@ -24,6 +30,19 @@
 
 <style lang="sass">
 .info
-  padding: 0 15px
+  padding: 0 1.5em
   margin-bottom: 1.5em
+
+.work
+  &__info
+    padding-top: 0.5em
+    font-size: inherit
+
+    p
+      font-size: 0.9em
+      line-height: 1.2em
+      margin-top: 0.3em
+
+  .list__item
+    margin-top: 1em
 </style>
