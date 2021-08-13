ce6ded16cc0e36ed434fc78a415bebcfb077c4be
Viacheslav Ivanov <brightsdayss@gmail.com>
2021-08-13 23:44:44 +0200

    generate

--- dist/_nuxt/static/1627922930/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Farticles\u002Fold-school-deck-dead-guy-ale","\u002Farticles\u002Fold-school-deck-rabid-wombat","\u002Farticles\u002Fretro-decktech-warp-world-t2","\u002Farticles\u002Fstandard-mono-black-aggro","\u002Farticles\u002Fos-mtg-kak-nachaty-igrat","\u002Farticles\u002Fmodern-fetchless-gifts-storm"]})
