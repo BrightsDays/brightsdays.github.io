f6f1c198d6d61ad81519559d61da8ca0bba7f59d
Viacheslav Ivanov <brightsdayss@gmail.com>
2021-08-17 21:23:16 +0200

    add articles

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/articles/os-mtg-kak-nachaty-igrat',
     name: 'Old School MTG: Как начать играть?',
     component: () => import('../views/articles/os-mtg-kak-nachaty-igrat.vue')
+  },
+  {
+    path: '/articles/old-school-deck-dead-guy-ale',
+    name: 'Old School Deck: Dead Guy Ale',
+    component: () => import('../views/articles/old-school-deck-dead-guy-ale.vue')
   }
 ];
 
