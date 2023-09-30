3aa8076a58187eee4a1b0a095b3f5e4ef44fc58c
vivanov <brightsdayss@gmail.com>
2023-09-30 19:36:14 +0400

    add deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import react from '@astrojs/react'
 import node from '@astrojs/node'
 
 export default defineConfig({
+  site: 'https://brightsdays.github.io',
   scopedStyleStrategy: 'class',
   output: 'hybrid',
   adapter: node({
